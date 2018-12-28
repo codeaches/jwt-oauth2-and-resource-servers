29513bbea7ab1d6fb47b4c81099cb3f76093668f
codeaches <pavan@codeaches.com>
2018-12-27 23:22:49 -0700

    no message

--- jwtoauth2server/src/main/java/com/codeaches/jwtoauth2server/AuthServerConfig.java
+++ jwtoauth2server/src/main/java/com/codeaches/jwtoauth2server/AuthServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -24,6 +25,9 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
 	AuthenticationManager authMgr;
+
+	@Autowired
+	private UserDetailsService usrSvc;
 
 	@Bean("clientPasswordEncoder")
 	PasswordEncoder clientPasswordEncoder() {
@@ -60,5 +64,6 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
 		endpoints.accessTokenConverter(jwtAccessTokenConverter());
 		endpoints.authenticationManager(authMgr);
+		endpoints.userDetailsService(usrSvc);
 	}
 }
